73b06b0135f3072b66ca456e3ec3d7da8524989e
yefdev96ys <yeferdev.10yps@gmail.com>
2023-05-26 10:03:53 -0500

    fourth commit markup 80%

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,21 @@
 import styles from './styles.module.css'
 import tuprutec from '../../assets/Tu PruTec.svg'
+import arrowicon from '../../assets/Arrow Down.svg'
 
 const Header = (): JSX.Element => {
   return (
     <div className={styles.content_header}>
-      <div>
+      <div className={styles.content_header_div}>
         <img 
         src={tuprutec}
         alt="logotuPrutec"
         />
+        <div className={styles.content__header_divS}>
+        <h2>Nombre de usuario</h2>
+        <button className={styles.content_header_button}>
+          <img src={arrowicon} alt="iconsvgarrow" />
+          </button>
+        </div>
       </div>
     </div>
   )
