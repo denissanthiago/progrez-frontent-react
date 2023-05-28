b8a937e7b79e7d02faf65911242635778a01aca4
yefdev96ys <yeferdev.10yps@gmail.com>
2023-05-28 15:51:57 -0500

    unit test for Headers read

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Header from './index'
 
 it("test_header_is_responsive_and_adapts_to_screen_sizes", () => {
@@ -7,3 +7,8 @@ it("test_header_is_responsive_and_adapts_to_screen_sizes", () => {
     const h2 = wrapper.container.querySelector('h2')
     expect(h2?.textContent).toBe('Nombre de Usuario')
 });
+it("test_images_render_correctly", () => {
+    render(<Header />);
+    const logo = screen.getAllByAltText("logotuPrutec");
+    expect(logo.length).toBe(1);
+  });
